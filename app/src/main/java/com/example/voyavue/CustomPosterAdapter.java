5ed0e15923ecec336a27e4d8847140837206498a
meetm7 <matharu.meet1994@gmail.com>
2020-11-26 02:54:38 -0800

    HomePage Skeleton Ready!

---
+++ app/src/main/java/com/example/voyavue/CustomPosterAdapter.java
@@ -0,0 +1,75 @@
+package com.example.voyavue;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.voyavue.models.Post;
+
+import java.util.List;
+import java.util.TooManyListenersException;
+
+public class CustomPosterAdapter extends RecyclerView.Adapter<CustomPosterAdapter.CustomViewHolder> {
+
+    List<Post> postLists;
+    Context context;
+
+    public CustomPosterAdapter(List<Post> passedPostLists){
+        this.postLists = passedPostLists;
+    }
+
+    public void ChangeData(List<Post> passedPostLists){
+        this.postLists = passedPostLists;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View postView = layoutInflater.inflate(R.layout.layout_post, parent, false);
+        CustomViewHolder customViewHolder = new CustomViewHolder(postView);
+        return customViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+        holder.txtViewPostOwnerUserName.setText(postLists.get(position).component1());
+        holder.txtViewPostTitle.setText(postLists.get(position).component3());
+        holder.txtViewPostDescription.setText(postLists.get(position).component5());
+    }
+
+    @Override
+    public int getItemCount() {
+        return postLists.size();
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder{
+        ImageView imgViewPostOwnerPhoto, imgViewPostPicture;
+        TextView txtViewPostTitle, txtViewPostOwnerUserName, txtViewPostDescription;
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgViewPostOwnerPhoto = itemView.findViewById(R.id.imgViewPostOwnerPhoto);
+            imgViewPostPicture = itemView.findViewById(R.id.imgViewPostPicture);
+
+            txtViewPostOwnerUserName = itemView.findViewById(R.id.txtViewPostOwnerUserName);
+            txtViewPostTitle = itemView.findViewById(R.id.txtViewPostTitle);
+            txtViewPostDescription = itemView.findViewById(R.id.txtViewPostDescription);
+
+            imgViewPostPicture.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context, "Opening Post!", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/voyavue/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/voyavue/ui/home/HomeFragment.java
@@ -1,46 +1,83 @@
 package com.example.voyavue.ui.home;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.PointerIcon;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.voyavue.CustomPosterAdapter;
 import com.example.voyavue.LoginActivity;
+import com.example.voyavue.MainActivity;
 import com.example.voyavue.R;
+import com.example.voyavue.models.Post;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiresApi(api = Build.VERSION_CODES.R)
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private RecyclerView recyclerView;
+    private final Post post = new Post("Meet_m7",
+            "0",
+            "Image Title",
+            1,
+            "This is post description",
+            "Tag1",
+            "Delhi",
+            "7PM",
+            "700$",
+            true,
+            true);
+    private final List<Post> allPosts = new ArrayList();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getContext(), LoginActivity.class));
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+//
+//        textView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FirebaseAuth.getInstance().signOut();
+//                startActivity(new Intent(getContext(), LoginActivity.class));
+//            }
+//        });
+        allPosts.add(post);
+        allPosts.add(post);
+        allPosts.add(post);
+        allPosts.add(post);
+        allPosts.add(post);
+        allPosts.add(post);
+        final CustomPosterAdapter customPosterAdapter = new CustomPosterAdapter(allPosts);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(root.getContext());
+        recyclerView = root.findViewById(R.id.recyclerViewPosts);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(customPosterAdapter);
         return root;
     }
 }
