a7eda93dc7db0fda40acca2fdb887adf1d1fcf4e
meetm7 <matharu.meet1994@gmail.com>
2020-12-06 23:15:59 -0800

    Explore page changes

--- app/src/main/java/com/example/voyavue/ui/explore/ExploreFragment.java
+++ app/src/main/java/com/example/voyavue/ui/explore/ExploreFragment.java
@@ -1,5 +1,6 @@
 package com.example.voyavue.ui.explore;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,9 +14,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.voyavue.CustomGrid;
+import com.example.voyavue.CustomPosterAdapter;
+import com.example.voyavue.NewPostActivity;
 import com.example.voyavue.R;
 import com.example.voyavue.models.Post;
 
@@ -25,7 +29,9 @@ import java.util.List;
 public class ExploreFragment extends Fragment {
 
     private ExploreViewModel exploreViewModel;
-    private GridView gridView;
+    private RecyclerView recyclerView;
+
+    private CustomPosterAdapter.RecyclerViewClickListener mListner;
 
     private final List<Post> allPosts = new ArrayList();
 
@@ -35,17 +41,36 @@ public class ExploreFragment extends Fragment {
                 ViewModelProviders.of(this).get(ExploreViewModel.class);
         View root = inflater.inflate(R.layout.fragment_explore, container, false);
 
-        gridView = root.findViewById(R.id.gridView);
-        final CustomGrid customGrid = new CustomGrid(allPosts);
-        gridView.setAdapter(customGrid);
+        setOnClickListner();
+
+        recyclerView = root.findViewById(R.id.recyclerViewAllPosts);
+        final CustomPosterAdapter customPosterAdapter = new CustomPosterAdapter(allPosts, mListner);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(root.getContext());
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(customPosterAdapter);
 
         exploreViewModel.getPosts().observe(getViewLifecycleOwner(), new Observer<ArrayList<Post>>() {
             @Override
             public void onChanged(ArrayList<Post> posts) {
-                customGrid.ChangeData(posts);
+                customPosterAdapter.ChangeData(posts);
             }
         });
 
         return root;
+    }
+
+
+    private void setOnClickListner() {
+        mListner = new CustomPosterAdapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View v, String id) {
+                Intent i = new Intent(v.getContext(), NewPostActivity.class);
+                i.putExtra("postId", id);
+                i.putExtra("isEditable", true);
+
+                startActivity(i);
+            }
+        };
     }
 }
