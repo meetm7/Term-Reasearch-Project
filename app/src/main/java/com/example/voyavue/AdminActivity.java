35c630c34854b3eb0db8c8f411ffb0a5dc7e89e6
meetm7 <matharu.meet1994@gmail.com>
2020-12-07 02:14:45 -0800

    Admin page Added!

--- app/src/main/java/com/example/voyavue/AdminActivity.java
+++ app/src/main/java/com/example/voyavue/AdminActivity.java
@@ -2,13 +2,37 @@ package com.example.voyavue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AdminActivity extends AppCompatActivity {
+
+    Button btnAddVerifiedPost;
+    TextView textViewNumOfUsers, textViewAvgPostPerUser, textViewMaleUsers, textViewFemaleUsers;
+    TextView textViewTotalNumberOfPosts, textViewAvgNumOfLikes, textViewNumOfVerifiedPosts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        btnAddVerifiedPost = findViewById(R.id.btnAddVerifiedPost);
+        textViewNumOfUsers = findViewById(R.id.textViewNumOfUsers);
+        textViewAvgPostPerUser = findViewById(R.id.textViewAvgPostPerUser);
+        textViewMaleUsers = findViewById(R.id.textViewMaleUsers);
+        textViewFemaleUsers = findViewById(R.id.textViewFemaleUsers);
+        textViewTotalNumberOfPosts = findViewById(R.id.textViewTotalNumberOfPosts);
+        textViewAvgNumOfLikes = findViewById(R.id.textViewAvgNumOfLikes);
+        textViewNumOfVerifiedPosts = findViewById(R.id.textViewNumOfVerifiedPosts);
+
+        btnAddVerifiedPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminActivity.this, NewPostActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/voyavue/ui/explore/ExploreFragment.java
+++ app/src/main/java/com/example/voyavue/ui/explore/ExploreFragment.java
@@ -5,21 +5,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridLayout;
-import android.widget.GridView;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.voyavue.CustomGrid;
 import com.example.voyavue.CustomPosterAdapter;
 import com.example.voyavue.NewPostActivity;
 import com.example.voyavue.R;
